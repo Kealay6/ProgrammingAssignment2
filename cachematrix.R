5481fcbf9cb0a8df673a549c0f7dc01961c5f190
Kealay6 <58435212+Kealay6@users.noreply.github.com>
2020-01-05 15:00:31 -0500

    Applied solution to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## function that caches a special matrix where the argument will be a matrix
+makeCacheMatrix <- function(x=matrix()) {
+  
+  ## define a variable called "matrixInverse" and set it to be nothing (placeholder)
+  matrixInverse <- NULL
+  
+  ## function that sets y to x (original matrix)
+  set <- function(y) {
+    x <<- y
+    matrixInverse <<- NULL
+  }
+  
+  ## function that gets the value of x (original matrix)
+  get <- function() x 
+  
+  ## function that sets the value of the inverted matrix
+  setInverse <- function(inverse) matrixInverse <<- inverse
+  
+  ## function that gets the value of the inverted matrix
+  getInverse <- function() matrixInverse
+  
+  ## combine them all in a list
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## function that computes the inverse of the "special" matrix returned by makeCacheMatrix above
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #define a variable to get the inverse into
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    #if inv is NOT NULL, then this code executes
+    
+    #output a message to say it's getting the already calculated inverse
+    message("getting cached data")
+    
+    #return the answer (no need to calculate again)
+    return(inv)
+  }
+  
+  ## pull the original matrix and apply to the data variable
+  data <- x$get()
+  
+  ## calculate the inverse using the original matrix
+  inv <- solve(data,...)
+  
+  ## set the inverted matrix so that it can be cached for next time
+  x$setInverse(inv)
+  
+  ## return the answer
+  return(inv)
+  
 }
